1e50e41f02a1e9256ad919a700d448b66f401403
bbreanl <b.lewis1980@outlook.com>
2022-06-23 21:41:50 -0500

    push

---
+++ Project1_expression_alert/JS/project2_function/main.js
@@ -0,0 +1,8 @@
+ alert("Hello, World!")
+ var X = "Hello, World"
+ document.write(X);
+ function myFunction() {
+    var sentence = "I am the sun,";
+    sentence +="nice to meet you."
+    document.getElementById("hell").innerHTML = sentence;
+ }
