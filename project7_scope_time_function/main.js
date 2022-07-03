fb47b4cb6530c833c0809b1c0d2ccb924394ce21
bbreanl <b.lewis1980@outlook.com>
2022-07-02 20:14:13 -0500

    push

--- project7_scope_time_function/main.js
+++ project7_scope_time_function/main.js
@@ -1,5 +1,29 @@
-function get_Date() {
-    if (new Date () .getHours() < 18) {
-        document.getElementById("Greeting").innerHTML = "How are you today?"
-    }
+var X = 10;
+function Add_numbers_1() {
+    document.write(20 + X + "<br>");
 }
+function Add_numbers_2() {
+    document.write(x + 100);
+}
+Add_numbers_1;
+Add_numbers_2;
+
+function Add_numbers_1(){
+    var X = 10;
+    document.write(20 + X + "<br>")
+}
+function Add_numbers_2() {
+    document.write(X + 100);
+}
+Add_numbers_1();
+Add_numbers_2();
+
+function Add_numbers_1() {
+    var X = 10;
+    console.log(15 + X);
+}
+function Add_numbers_2() {
+    console.log(X + 100);
+}
+Add_numbers_1();
+Add_numbers_2();
