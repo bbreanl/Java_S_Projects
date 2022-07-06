61e542aa094f0c226a83fea0f19b3ddbd951488a
bbreanl <b.lewis1980@outlook.com>
2022-07-05 23:01:10 -0500

    push

---
+++ project8_string_method/main.js
@@ -0,0 +1,15 @@
+function slice_Method() {
+    var Sentence = "All work and no play makes Johnny a dull boy.";
+    var Section = Sentence.slice(27,33);
+    document.getElementById("Slice").innerHTML = Section;
+}
+
+function string_Method() {
+    var X =182;
+    document.getElementById("Numbers_to_string").innerHTML = X.toString();
+}
+
+function precision_Method() {
+    var X = 12938.3012987376112;
+    document.getElementById("Percision").innerHTML = X.toPrecision(10);
+}
